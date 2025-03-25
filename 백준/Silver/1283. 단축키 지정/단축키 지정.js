9206b115fd56e914d89d0f45629945033ed9d30f
silverain02 <108103346+silverain02@users.noreply.github.com>
2025-03-25 10:32:14 +0900

    [Silver I] Title: 단축키 지정, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.js
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.js
@@ -1,57 +1,55 @@
-// 입력을 받아오기 위해 아래 두줄의 코드를 사용하세요. (수정 금지)
 const fs = require('fs');
-const stdin = fs.readFileSync('/dev/stdin').toString().trim().split(`\n`);
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = Number(input[0]);
+const words = input.slice(1);
 
-const n = Number(stdin[0]);
-const words = stdin.slice(1);
-// console.log(n,words)
+function assignShortcuts(words) {
+    const usedShortcuts = new Set();
+    const shortcutIndices = [];
 
-//초기화
-const set = new Set();
-const stack = [];
+    // 첫 글자 단축키 우선 탐색
+    for (let i = 0; i < words.length; i++) {
+        const wordParts = words[i].split(' ');
+        let shortcutFound = false;
 
-//단어 순환
-words.forEach(word=>{
-    // console.log([...set],word);
-    const word_arr = word.split(' ');
-    // console.log(word_arr);
-    //단어 단위 순환
-    let isValid = false;
-    for(let i=0; i<word_arr.length; i++){
-        // console.log(word_arr[i]);
-        const w = word_arr[i];
-        // console.log(w[0])
-        if(!set.has(w[0].toLowerCase())){
-            set.add(w[0].toLowerCase());
-            // console.log(w[0].toLowerCase())
-            if(i==0) stack.push(0)
-            else stack.push(word_arr.slice(0,i).join().length+1);
-            // console.log(word_arr.slice(0,i).join().length)
-            isValid = true
-            break;
-        }
-    }
-    if(!isValid){
-        // console.log("땡", word);
-        //전체 탐색
-        for(let j=0;j<word.length; j++){
-            // console.log(word[j])
-            if(word[j]!==' '&&!set.has(word[j].toLowerCase())){
-                set.add(word[j].toLowerCase());
-                stack.push(j);
-                isValid = true
+        // 각 단어의 첫 글자 탐색
+        for (let j = 0; j < wordParts.length; j++) {
+            const firstChar = wordParts[j][0].toLowerCase();
+            if (!usedShortcuts.has(firstChar)) {
+                usedShortcuts.add(firstChar);
+                shortcutIndices.push(j === 0 ? 0 : words[i].split(' ').slice(0, j).join(' ').length + 1);
+                shortcutFound = true;
                 break;
             }
         }
+
+        // 첫 글자 단축키 실패 시 전체 문자 탐색
+        if (!shortcutFound) {
+            for (let j = 0; j < words[i].length; j++) {
+                const char = words[i][j];
+                if (char !== ' ' && !usedShortcuts.has(char.toLowerCase())) {
+                    usedShortcuts.add(char.toLowerCase());
+                    shortcutIndices.push(j);
+                    shortcutFound = true;
+                    break;
+                }
+            }
+        }
+
+        // 단축키를 찾지 못한 경우
+        if (!shortcutFound) {
+            shortcutIndices.push(-1);
+        }
     }
-    if(!isValid) stack.push(-1)
-})
-// console.log('최종',stack)
-    // 출력 처리
-    let t = 0
-    words.forEach(w=>{
-        const arr = w.split('');
-        if(stack[t]!== -1) arr[stack[t]] = `[${arr[stack[t]]}]`
-        console.log(arr.join(''))
-        t++
-    })
+
+    // 단축키 표시
+    return words.map((word, index) => {
+        if (shortcutIndices[index] === -1) return word;
+        const chars = word.split('');
+        chars[shortcutIndices[index]] = `[${chars[shortcutIndices[index]]}]`;
+        return chars.join('');
+    });
+}
+
+const result = assignShortcuts(words);
+console.log(result.join('\n'));
