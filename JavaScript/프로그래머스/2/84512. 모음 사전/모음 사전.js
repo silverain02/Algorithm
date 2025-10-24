3daea65880c59fb975a1e2889bd194b7aafcc895
이은비 <108103346+silverain02@users.noreply.github.com>
2025-10-24 15:01:57 +0900

    [level 2] Title: 모음 사전, Time: 5.55 ms, Memory: 37.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ JavaScript/프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,14 @@
 function solution(word) {
-    //하드코딩 규칙
-    const alp = ['A','E','I',"O","U"]
-    const arr =[781,156,31,6,1];
-    let answer = 0;
-    answer += word.length;
-    
-    for(let i=0; i<word.length; i++){
-        const diff = arr[i] * alp.indexOf(word[i]) +1
-        answer += arr[i]* alp.indexOf(word[i]) 
+    const words = [];
+    const vowels = ['A', 'E', 'I', 'O', 'U'];
+
+    function dfs(cur) {
+      if (cur.length > 5) return;
+      if (cur.length > 0) words.push(cur);
+        for (let v of vowels) dfs(cur + v);
     }
-    return answer
+    dfs('');
+    words.sort();
+    return words.indexOf(word) + 1;
+
 }
