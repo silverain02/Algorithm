b72f61836f4ea52b4cb6dc0f6c062330ff62e250
이은비 <108103346+silverain02@users.noreply.github.com>
2026-01-02 16:31:07 +0900

    [level 2] Title: 괄호 회전하기, Time: 126.55 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,30 +1,26 @@
-function isValid(s) {
-    const stack = [];
-    const map = { ')': '(', '}': '{', ']': '[' };
-    
-    for (let char of s) {
-        if (char === '(' || char === '{' || char === '[') {
-            stack.push(char);
-        } else {
-            if (stack.length === 0 || stack.pop() !== map[char]) {
-                return false;
+function solution(s) {
+    //0. 초기화
+    let answer = 0;
+    //1. 시작 index잡기 N
+    for(let i=0; i<s.length; i++){
+        const stack = []
+        //1-1. s 돌기 N
+        for(let j=i; j<i+s.length; j++){
+            const v = s[j%s.length];
+            //1-1-1. stack 쌓기
+            stack.push(v);
+            //1-1-2. stack 제거
+            let top = stack[stack.length-1];
+            let prev = stack[stack.length-2];
+            while((prev=='[' && top==']') || (prev=='{' && top=='}') || (prev=='(' && top==')')){
+                stack.pop();
+                stack.pop();
+                top = stack[stack.length-1];
+                prev = stack[stack.length-2];
             }
         }
+        //1-2. 올바른 체크
+        if(stack.length == 0) answer ++;
     }
-    
-    return stack.length === 0;
-}
-
-function solution(s) {
-    let result = 0;
-    let rotated = s;
-    
-    for (let i = 0; i < s.length; i++) {
-        if (isValid(rotated)) {
-            result++;
-        }
-        rotated = rotated.slice(1) + rotated[0]; // 왼쪽으로 회전
-    }
-    
-    return result;
+    return answer
 }
