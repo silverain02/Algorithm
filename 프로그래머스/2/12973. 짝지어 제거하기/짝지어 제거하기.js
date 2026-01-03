d5ffa6163fc96bf227efda8daaea86bf7ff21994
이은비 <108103346+silverain02@users.noreply.github.com>
2026-01-03 15:17:35 +0900

    [level 2] Title: 짝지어 제거하기, Time: 28.01 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,13 +1,16 @@
-function solution(s){
-	const stack = [];
-	
-	for(const c of s){
-		//스택이 비어있지 않고 현재 문자와 스택의 맨 위 문자가 같으면
-		if(stack.length>0 && stack[stack.length-1]==c){
-			stack.pop(); //스택의 맨 위 문자 제거
-		}else{
-			stack.push(c); //스택에 현 문자 추가
-		}
-	}
-	return stack.length ===0?1:0;
+function solution(s)
+{
+    const stack = []
+    //1. s 노드 순회
+    for(let i=0; i<s.length; i++){
+        const v = s[i];
+        
+        //1-1. stack pop/push 조건 체크
+        if(stack.length>=1 && stack[stack.length-1]==v){
+            stack.pop()
+        }else{
+            stack.push(v)
+        }
+    }
+    return stack.length==0?1:0
 }
