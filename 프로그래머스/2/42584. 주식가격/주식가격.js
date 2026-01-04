11eea90d40713ef19eb52890cafc6ac41623afda
이은비 <108103346+silverain02@users.noreply.github.com>
2026-01-04 16:46:20 +0900

    [level 2] Title: 주식가격, Time: 29.30 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,21 +1,21 @@
 function solution(prices) {
-    const N = prices.length;
-    const answer = new Array(N).fill(0);
-    const stack = [];
-
-    for (let i = 0; i < N; i++) {
-        // 현재 가격이 이전 가격보다 떨어졌을 때
-        while (stack.length && prices[i] < prices[stack[stack.length - 1]]) {
-            const top = stack.pop();
-            answer[top] = i - top;
+    const n = prices.length;
+    const answer = new Array(n).fill(0) //1. 가격이 떨어지지 않은 기간 저장 배열
+    //stack 으로 이전가격과 현재 가격 비교
+    const stack = [0] //2. 스택 초기화
+    for(let i=1; i<n; i++){
+        while(stack.length>0 && prices[i] < prices[stack[stack.length-1]]){
+            //3. 가격이 떨어졌으므로 이전 가격의 기간을 계산
+            const j = stack.pop();
+            answer[j] = i-j;
         }
-        stack.push(i);
+        stack.push(i)
     }
-
-    // 끝까지 안 떨어진 경우 처리
-    while (stack.length) {
-        const top = stack.pop();
-        answer[top] = N - 1 - top;
+    //4. 스택에 남아있는 가격들은 가격이 떨어지지 않는 경우
+    while(stack.length>0){
+        const j = stack.pop();
+        answer[j] = n-1-j;
     }
-    return answer;
+    
+    return answer
 }
