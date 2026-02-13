39b6173d1996cf5c8723efe95ed9e70b741aca73
이은비 <108103346+silverain02@users.noreply.github.com>
2026-02-13 11:35:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 31.93 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,31 +1,29 @@
 function solution(maps) {
-    const queue = [[0, 0, 1]];
-    const [n, m] = [maps.length, maps[0].length];
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
-    const visited = Array.from({ length: n }, () => Array(m).fill(false));
-    visited[0][0] = true;
-
-    let front = 0;
-    while (front < queue.length) {
-        const [i, j, depth] = queue[front++];
-        if (i === n - 1 && j === m - 1) {
-            return depth;
-        }
-
-        for (let t = 0; t < 4; t++) {
-            const ni = i + dx[t];
-            const nj = j + dy[t];
-
-            if (
-                ni >= 0 && nj >= 0 && ni < n && nj < m &&
-                maps[ni][nj] === 1 && !visited[ni][nj]
-            ) {
-                visited[ni][nj] = true; // ✅ 큐에 넣기 전에 방문 처리
-                queue.push([ni, nj, depth + 1]);
+    //1. N,M구하기
+    const [N,M] = [maps.length, maps[0].length];
+    //2. BFS 돌리기
+    const queue = [[0,0,1]];
+    const visited = new Set();
+    visited.add('0,0')
+    let head = 0;
+    const di = [-1,1,0,0];
+    const dj = [0,0,-1,1];
+    let minT = Infinity;
+    while(head<queue.length){
+        //pop
+        const v = queue[head];
+        //도착 확인
+        // console.log('target:',v)
+        if(v[0]==N-1 && v[1]==M-1) return v[2];
+        head++;
+        //이동
+        for(let k=0; k<4; k++){
+            const [ni,nj,nt] = [v[0]+di[k],v[1]+dj[k],v[2]+1]
+            if(ni>=0 && ni<N && nj>=0 && nj<M && maps[ni][nj]==1 && !visited.has([ni,nj].join(',')) ){
+                queue.push([ni,nj,nt])
+                visited.add([ni,nj].join(','))
             }
         }
     }
-
     return -1;
 }
