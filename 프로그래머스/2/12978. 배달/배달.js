aa281211de3ddb240201e15a94fc1942b98dd6bd
이은비 <108103346+silverain02@users.noreply.github.com>
2026-02-18 18:01:58 +0900

    [level 2] Title: 배달, Time: 7.35 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,32 +1,42 @@
 function solution(N, road, K) {
-    const graph = Array.from({ length: N + 1 }, () => []);
-    const distances = Array(N + 1).fill(Infinity);
-    distances[1] = 0;
-
-    // 그래프 구성
-    for (const [a, b, cost] of road) {
-        graph[a].push([b, cost]);
-        graph[b].push([a, cost]);
+    //1. 인접행렬 map 생성
+    const map = new Map();
+    for(let i=1; i<=N; i++){
+        const nMap = new Map()
+        map.set(i,nMap);
     }
-
-    const queue = [[0, 1]]; // [거리, 노드]
-
-    while (queue.length > 0) {
-        // 거리 기준으로 정렬
-        queue.sort((a, b) => a[0] - b[0]);
-        const [dist, node] = queue.shift(); // 가장 가까운 노드
-
-        // 이미 더 짧은 경로로 방문했다면 skip
-        if (dist > distances[node]) continue;
-
-        for (const [nextNode, nextDist] of graph[node]) {
-            const cost = dist + nextDist;
-            if (cost < distances[nextNode]) {
-                distances[nextNode] = cost;
-                queue.push([cost, nextNode]);
+    //1-1. road돌며 인접 map update
+    for(const r of road){
+        const [a,b,w] = r;
+        if(map.get(a).has(b) && map.get(a).get(b) <= w) continue
+        map.get(a).set(b,w)
+        map.get(b).set(a,w)
+    }
+    //2. 다익스트라 
+    const board = new Map();
+    for(let i=2; i<=N; i++){
+        board.set(i,Infinity)
+    }
+    //2-1. DFS돌기
+    const stack = [[1,0]];
+    while(stack.length!==0){
+        const [v,w] = stack.pop();
+        const nmap = map.get(v);
+        //board update
+        board.set(v,w)
+        //move
+        for(const k of nmap.keys()){
+            if(k!==1 && board.get(k)>w+nmap.get(k) && K>=w+nmap.get(k)){
+                stack.push([k,w+nmap.get(k)]);
             }
         }
     }
-
-    return distances.filter((d) => d <= K).length;
+    console.log(board)
+    //3. 결과
+    let result = 0;
+    for(const k of board.keys()){
+        if(board.get(k) <= K) result++
+    }
+    return result
+    
 }
